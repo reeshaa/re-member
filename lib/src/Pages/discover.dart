4b8304f48be56a03d5656ee2c0bc19d0fa75ebc6
SHARONLOBO <sharonjudithlobo@gmail.com>
2021-10-02 14:09:07 +0530

    added pages

---
+++ lib/src/Pages/discover.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class Discover extends StatefulWidget {
+  const Discover({ Key? key }) : super(key: key);
+
+  @override
+  _DiscoverState createState() => _DiscoverState();
+}
+
+class _DiscoverState extends State<Discover> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/src/Pages/leaderboard.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class LeaderBoard extends StatefulWidget {
+  const LeaderBoard({ Key? key }) : super(key: key);
+
+  @override
+  _LeaderBoardState createState() => _LeaderBoardState();
+}
+
+class _LeaderBoardState extends State<LeaderBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
