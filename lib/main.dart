d8606630353ba7fa21040efaa39049d55a4a0303
SHARONLOBO <sharonjudithlobo@gmail.com>
2021-10-02 13:32:22 +0530

    pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:re_member/src/Pages/home.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
@@ -17,5 +16,5 @@ void main() async {
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
-  runApp(home());
+  runApp(MyApp(settingsController: settingsController));
 }
